9cfdf6fbf1f8de62f78b085060935434b8a23c73
rkrug5 <phishermankrug@hotmail.com>
2018-05-29 02:06:10 -0700

    working handlebars

--- server.js
+++ server.js
@@ -28,5 +28,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/week18Populater");
+const exphbs = require("express-handlebars");
+
+
+app.engine('handlebars', exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+// Routes
+// ===========================================================
+
+app.get("/", function (req, res) {
+	res.render("landing");
+});
+
+
+
+
+
+app.listen(PORT, function () {
+	console.log("App running on port " + PORT + "!");
+
+	app.get("/", function (req, res) {
+		res.render("landing");
+	});
+});
